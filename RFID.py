9daf3e142305a3ac0dfd6b963a095d8da4ba7d1e
Skipper-is <skipperthepilot@googlemail.com>
2018-02-04 14:20:26 +0000

    Lambs & Ewes implemented, I think! No save or clear though!

--- RFID.py
+++ RFID.py
@@ -3,15 +3,19 @@ import re
 from tkinter import *
 import threading
 import winsound
+import os.path
 
 # variables list
 window = Tk()
 threads = []
 shutdown = False
+ewe_selected_String=StringVar()
+scanningSwitch = 0
+selected_ewe = ""
 #
 seen = set()
 serialPort = serial.Serial()
-
+csv_path = "list.csv"
 
 # functions list
 def now():
@@ -28,21 +32,34 @@ def remove_duplicates(inputlist):
         if value not in seen and len(value) > 4:
             output.append("UK" + value)
             seen.add(value)
-            write_toCSV(value)
+            #see if there is a selected ewe:
+            if(len(selected_ewe)>0):
+                #then we've got lambs - add the selected ewe bit to the lambs
+                write_toCSV(value,selected_ewe)
+            elif (len(selected_ewe)==0):
+                write_toCSV(value,"Ewe")
+                #then we're looking at the ewe, so screw everything!
+
+            #write_toCSV(value)
             frequency = 2000
             duration = 300
             winsound.Beep(frequency, duration)
+
         elif value in seen and len(value) > 4:
             frequency = 1000
             duration = 500
             winsound.Beep(frequency, duration)
+            print(selected_ewe)
     return output
 
 
-def write_toCSV(value):
+def write_toCSV(value, ewe):
+    if  not os.path.isfile(csv_path):
+        with open(csv_path, "w") as f:
+            f.write("Tag Number, Time, Mother\n")
     with open("list.csv", "a") as f:
         if len(value) > 4:
-            f.write("UK" + value + "," + now() + "\n")
+            f.write("UK" + value + "," + now() + ","+ewe +"\n")
 
 
 def split_string(i):
@@ -51,7 +68,7 @@ def split_string(i):
         for items in splitlist:
             if len(items) < 4:
                 splitlist.remove(items)
-                return splitlist
+        return splitlist
     else:
         return "error"
 
@@ -94,10 +111,25 @@ def read_data(serial_port):
                 splitlist = split_string(response)
                 splitlist = remove_duplicates(splitlist)
                 if len(splitlist) > 0:
-                    ewes.insert(END, splitlist[-1])
+                    if scanningSwitch == 0:
+                        ewes.insert(END, splitlist[-1])
+
+                    if scanningSwitch == 1:
+                        lambs.insert(END, splitlist[-1])
+
 
             if shutdown:
                 break
+
+def select_ewe(event):
+    selection =ewes.curselection()
+    global selected_ewe
+    selected_ewe=ewes.get(selection[0])
+    print(selected_ewe)
+    ewe_selected_String.set(selected_ewe)
+    global scanningSwitch
+    scanningSwitch = 1
+
 
 
 def submit():
@@ -119,11 +151,19 @@ if len(comslist) > 0:
     drop1.grid(row=0, column=0)
 
 if len(comslist) == 0:
-    w = Label(window, text="No connections found")
-    w.grid(row=0, column=0)
+    noneFound = Label(window, text="No connections found")
+    noneFound.grid(row=0, column=0)
 
 ewes = Listbox(window, height=5, width=40, selectmode = "SINGLE")
 ewes.grid(row=1, column=1)
+
+ewes.bind("<Double-Button-1>", select_ewe)
+
+ewe_selected = Label(window, textvariable=ewe_selected_String)
+ewe_selected_String.set("None selected (double click)")
+ewe_selected.grid(row=0, column=4)
+selected_title= Label(window,text="Ewe:")
+selected_title.grid(row=0, column=3)
 
 lambs = Listbox(window, height=5, width=40)
 lambs.grid(row=1, column=4)
