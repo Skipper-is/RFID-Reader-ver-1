d9a0a8bdae4f145da115451b800ac55eef7feb43
Skipper-is <skipperthepilot@googlemail.com>
2018-02-04 11:21:20 +0000

    Scans & saves tag numbers. Nothing more, nothing less.

--- RFID.py
+++ RFID.py
@@ -1,13 +1,15 @@
 import serial
 import re
 from tkinter import *
-
+import threading
+import winsound
 #variables list
 window = Tk()
-fulllist = []
-ser = serial.Serial( port = input("Enter comm number:"), timeout=1)
+threads = []
+shutdown = False
+#
 seen = set()
-
+serialPort = serial.Serial()
 #functions list
 def now():
     import datetime
@@ -18,11 +20,19 @@ def now():
 def remove_duplicates(inputlist):
     output = []
 
+
     for value in inputlist:
         if (value not in seen and len(value)>4):
-            output.append(value)
+            output.append("UK"+value)
             seen.add(value)
             write_toCSV(value)
+            frequency = 2000
+            duration = 300
+            winsound.Beep(frequency,duration)
+        elif (value in seen and len(value)>4):
+            frequency=1000
+            duration=500
+            winsound.Beep(frequency,duration)
     return output
 
 def write_toCSV(value):
@@ -30,11 +40,6 @@ def write_toCSV(value):
         if len(value)>4:
             f.write("UK"+value+","+now()+"\n")
 
-def add_to_list(item):
-	if item not in fulllist:
-		fulllist.append(item)
-
-	return fulllist
 
 def split_string(i):
     if i != "":
@@ -46,18 +51,71 @@ def split_string(i):
     else:
         return "error"
 
+def serial_ports():
+    if sys.platform.startswith('win'):
+        #Windows based OS, we're happy!
+        ports=['COM%s'%(i+1)for i in range(256)]
+    else:
+        raise EnvironmentError('Unsupported platform!')
+    result = []
+    for port in ports:
+        try:
+            s=serial.Serial(port)
+            s.close()
+            result.append(port)
+        except (OSError, serial.SerialException):
+            pass
+
+    return result
+
+def connectComPort(event):
+
+    with serial.Serial(port = drop1_vars.get(), timeout=1) as ser:
+        print("success!")
+        ser.close()
+        t=threading.Thread(target=readData, args=(drop1_vars.get(),))
+        threads.append(t)
+        t.start()
+
+
+
+
+
+def readData(serialPort):
+
+    with serial.Serial(port=serialPort, timeout=1) as ser:
+        SerialPort = ser
+        while True:
+            response = ser.readline()
+            response=response.decode("utf-8")
+
+            if response != "":
+                splitlist = split_string(response)
+                splitlist = remove_duplicates(splitlist)
+                if len(splitlist)>0:
+                    t1.insert(END,splitlist[-1])
+
+            if(shutdown==True):
+                break
+
+
+
+
 window.title("RFID Reader ver 1")
+#Lets make some items!
+drop1_vars = StringVar()
+comslist = serial_ports()
+print(comslist)
+if len(comslist) > 0:
+    drop1_vars.set(comslist[0])
+    drop1 = OptionMenu(window, drop1_vars, *comslist, command=connectComPort)
+    drop1.grid(row=0, column=0)
 
+t1=Listbox(window, height=5, width=40)
+t1.grid(row=0,column=2,rowspan=2)
+print(serial_ports())
 #Main script/body
-while True:
-    response = ser.readline()
-    response=response.decode("utf-8")
-
-    if response != "":
-        splitlist = split_string(response)
-        splitlist = remove_duplicates(splitlist)
-        print(seen)
 
 
-ser.close()
 window.mainloop()
+shutdown = True
